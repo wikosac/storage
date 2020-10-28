b4f3fbcd23eb6058a8963106ebd0f7c1501f371e
Dwiko Indrawansyah <wesibrani@gmail.com>
2020-10-28 21:51:50 +0700

    HelloWorld.java

---
+++ Praktikum 1/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+
+	public statis void main(String[] args) {
+		System.out.printIn("Hello Wiko!");
+
+		System.out.printIn("Nama : Dwiko Indrawansyah");
+		System.out.printIn("Panggilan : Wiko");
+		System.out.printIn("Nim : 6706204097");
+
+	}
+}
